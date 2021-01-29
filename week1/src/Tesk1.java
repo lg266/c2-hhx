1deb255f16153b2aac365d337b26edfbf5654f2a
lg266 <15968651528@163.com>
2021-01-29 12:36:39 +0800

    作业

--- week1/src/Tesk1.java
+++ week1/src/Tesk1.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+import static java.lang.Math.pow;
+
 public class Tesk1 {
     public static void main(String[] args) {
-        System.out.println("hello world!");
+        double res;
+        Scanner in=new Scanner(System.in);
+        System.out.print("输入体重(kg):");
+        int n=in.nextInt();
+        System.out.print("输入身高(m):");
+        double m=in.nextDouble();
+        res=n/pow(m,2);
+        if(res<18.5){
+            System.out.println("过轻");
+        }
+        else if(res<25){
+            System.out.println("正常");
+        }
+        else if(res<28){
+            System.out.println("过重");
+        }
+        else System.out.println("肥胖");
     }
 }
---
+++ week1/src/Tesk2.java
@@ -0,0 +1,2 @@
+public class Tesk2 {
+}
