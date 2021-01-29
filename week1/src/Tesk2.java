8ec553d4d419da8822f2150101617e8ba811ebf2
lg266 <15968651528@163.com>
2021-01-29 12:41:43 +0800

    作业

--- week1/src/Tesk2.java
+++ week1/src/Tesk2.java
@@ -1,2 +1,15 @@
+import static java.lang.Math.pow;
+
 public class Tesk2 {
+    public static void main(String[] args) {
+        int i,n;
+        n=0;
+        for(i=2;i<1000;i++){
+            if(i==(pow(i%10,3)+pow(i/10%10,3)+pow(i/100%10,3))) {
+                System.out.println(i);
+                n++;
+            }
+        }
+        System.out.println(n);
+    }
 }
